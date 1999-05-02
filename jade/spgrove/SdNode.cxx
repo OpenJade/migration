1a136f3482a74c4f61bdbe09ccdb4dc2faa7bf66
Avi <avi@users.sourceforge.net>
1999-05-02 12:57:24 +0000

    Initial revision

---
+++ jade/spgrove/SdNode.cxx
@@ -0,0 +1,16 @@
+// Copyright (c) 1997 James Clark
+// See the file COPYING for copying permission.
+
+
+#include "config.h"
+#include "SdNode.h"
+
+#ifdef SP_NAMESPACE
+namespace SP_NAMESPACE {
+#endif
+
+const Node::IID SdNode::iid = "SdNode";
+
+#ifdef SP_NAMESPACE
+}
+#endif

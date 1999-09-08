0e46e2c983bca8477b0d0e219c5bc36e1a366846
Unknown <pn@users.sourceforge.net>
1999-09-08 18:40:37 +0000

    Implemented char-map.

--- style/SosofoObj.h
+++ style/SosofoObj.h
@@ -116,10 +116,11 @@ public:
   virtual bool hasPseudoNonInheritedC(const Identifier *) const;
   virtual void setNonInheritedC(const Identifier *, ELObj *, const Location &, Interpreter &);
   // Char * is 0 if no implicit character.
-  virtual void setImplicitCharNICs(const Char *, const Location &,
-				   Interpreter &);
-  static void setImplicitCharNICs(const Char *, size_t,
-				  FOTBuilder::CharacterNIC *, Interpreter &);
+  virtual void setImplicitChar(Char);
+  static void fixCharNICs(const Char *, size_t,
+				  FOTBuilder::CharacterNIC *,
+				  const Location &,
+				  ProcessContext &);
   void traceSubObjects(Collector &) const;
   void setStyle(StyleObj *);
   void process(ProcessContext &);

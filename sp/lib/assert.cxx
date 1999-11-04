c21ea91d00333ace2b18cb8039bd4d726f4b7892
Matthias Clasen <clasen@users.sourceforge.net>
1999-11-04 08:36:18 +0000

    Initial revision, taken from the current jade repository at tag before_split.

---
+++ sp/lib/assert.cxx
@@ -0,0 +1,27 @@
+// Copyright (c) 1994 James Clark
+// See the file COPYING for copying permission.
+
+#ifdef SP_USE_OWN_ASSERT
+
+#include "splib.h"
+#include <stdlib.h>
+#include "macros.h"
+
+#ifdef __GNUG__
+void exit(int) __attribute__((noreturn));
+#endif
+
+#ifdef SP_NAMESPACE
+namespace SP_NAMESPACE {
+#endif
+
+void assertionFailed(const char *, const char *, int)
+{
+  abort();
+  exit(1);
+}
+
+#ifdef SP_NAMESPACE
+}
+#endif
+#endif /* SP_USE_OWN_ASSERT */

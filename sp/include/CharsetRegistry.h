bf69c38283ba7d45e0a0be8e7dfa71be7609c5d0
Matthias Clasen <clasen@users.sourceforge.net>
2000-01-20 21:15:09 +0000

    Apply patch for koi8 support from Growler@tula.net (Alexey V. Naidyonov).

--- sp/include/CharsetRegistry.h
+++ sp/include/CharsetRegistry.h
@@ -45,6 +45,7 @@ public:
     GB2312 = 58,
     ISO10646_UCS2 = 176,
     ISO10646_UCS4 = 177,
+    KOI8_R = 65534, // not registered
     BIG5 = 65535 // not registered
   };
   static ISORegistrationNumber getRegistrationNumber(const StringC &desig,

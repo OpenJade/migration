35bff7ebd2666a2fd42d92f748c2625554bd0af7
Matthias Clasen <clasen@users.sourceforge.net>
2000-02-07 17:07:27 +0000

    Next round of query cleanup. Move query rule implementation to ProcessingMode.cxx. Pattern.cxx is now in sync with the trunk again. This is completely unoptimized.

--- style/ProcessingMode.cxx
+++ style/ProcessingMode.cxx
@@ -9,6 +9,7 @@
 #include "Expression.h"
 #include "Insn.h"
 #include "Insn2.h"
+#include "VM.h"
 #include <OpenSP/IListIter.h>
 #include "LocNode.h"
 #include <OpenSP/macros.h>
@@ -21,20 +22,6 @@ namespace DSSSL_NAMESPACE {
 ProcessingMode::ProcessingMode(const StringC &name, const ProcessingMode *initial)
 : Named(name), initial_(initial), defined_(0)
 {
-  if (initial_)
-    hasQuery_ = initial_->hasQuery();
-  else 
-    hasQuery_ = 0;
-}
-
-void ProcessingMode::compile(Interpreter &interp)
-{
-  for (int i = 0; i < nRuleType; i++) {
-    for (size_t j = 0; j < rootRules_[i].size(); j++)
-      rootRules_[i][j].action().compile(interp, RuleType(i));
-    for (IListIter<ElementRule> iter(elementRules_[i]); !iter.done(); iter.next())
-      iter.cur()->action().compile(interp, RuleType(i));
-  }
 }
 
 void ProcessingMode::GroveRules::build(const IList<ElementRule> *lists,
@@ -88,6 +75,20 @@ int ruleCompare(const void *p1, const void *p2)
 void ProcessingMode::GroveRules::sortRules(Vector<const ElementRule *> &v)
 {
   qsort(&v[0], v.size(), sizeof(v[0]), ruleCompare);
+}
+
+void ProcessingMode::compile(Interpreter &interp)
+{
+  for (int i = 0; i < nRuleType; i++) {
+    for (size_t j = 0; j < rootRules_[i].size(); j++)
+      rootRules_[i][j].action().compile(interp, RuleType(i));
+    qsort(&(queryRules_[i][0]), queryRules_[i].size(), 
+          sizeof(queryRules_[i][0]), ruleCompare);  
+    for (size_t j = 0; j < queryRules_[i].size(); j++)
+      queryRules_[i][j]->action().compile(interp, RuleType(i));
+    for (IListIter<ElementRule> iter(elementRules_[i]); !iter.done(); iter.next())
+      iter.cur()->action().compile(interp, RuleType(i));
+  }
 }
 
 ProcessingMode::Action::Action(unsigned partIndex, Owner<Expression> &expr,
@@ -147,11 +148,102 @@ int ProcessingMode::ElementRule::compareSpecificity(const Rule &r) const
   return Pattern::compareSpecificity(*this, (const ElementRule &)r);
 }
 
+ProcessingMode::QueryRule::QueryRule(const Ptr<Action> &action,
+                                     Owner<Expression> &query,
+                                     Owner<Expression> &priority,
+				     ProcessingMode *pm,
+				     Interpreter *interp)
+: Rule(action), nl_(0), priority_(0), pm_(pm), interp_(interp), 
+Collector::DynamicRoot(*interp)
+{
+  nlExpr_.swap(query);
+  priorityExpr_.swap(priority);
+} 
+
+long ProcessingMode::QueryRule::priority() const
+{
+  if (priorityExpr_) {
+    InsnPtr insn = 
+       priorityExpr_->compile(*interp_, Environment(), 0, InsnPtr());
+    EvalContext ec;
+    VM vm(ec, *interp_);
+    ELObj *val = vm.eval(insn.pointer());
+    double tem;
+    if (!val || !val->realValue(tem)) {
+      interp_->setNextLocation(location());
+      interp_->message(InterpreterMessages::priorityNotNumber);
+      return 0;
+    }
+    if (!val->exactIntegerValue(
+#ifndef HAVE_MUTABLE
+                              ((QueryRule *)this)->
+#endif
+                              priority_)) {
+      interp_->setNextLocation(location());
+      interp_->message(InterpreterMessages::sorryPriority);
+      return 0;
+    }
+#ifndef HAVE_MUTABLE
+    ((QueryRule *)this)->
+#endif
+      priorityExpr_.clear();
+  }
+  return priority_;
+}
+
+bool ProcessingMode::QueryRule::matches(const NodePtr &nd) const
+{
+  NodePtr root;
+  //FIXME: what if nd happens not to be in the grove being processed ?
+  nd->getGroveRoot(root);
+  EvalContext ec;
+  EvalContext::CurrentNodeSetter cns(root, pm_, ec);
+  if (!nl_) {
+    InsnPtr insn = nlExpr_->compile(*interp_, Environment(), 0, InsnPtr());
+    VM vm(ec, *interp_);
+    ELObj *val = vm.eval(insn.pointer());
+    if (!val || !val->asNodeList()) {
+      interp_->setNextLocation(location());
+      interp_->message(InterpreterMessages::queryNotNodelist);
+      return 0;
+    }
+#ifndef HAVE_MUTABLE
+    ((QueryRule *)this)->
+#endif
+    nl_ = val->asNodeList();
+    interp_->makeReadOnly(
+#ifndef HAVE_MUTABLE
+      ((QueryRule *)this)->
+#endif
+      nl_);
+  }
+  return nl_->contains(ec, *interp_, nd);
+}
+
+int ProcessingMode::QueryRule::compareSpecificity(const Rule &r) const
+{
+  int result = Rule::compareSpecificity(r);
+  if (result)
+    return result;
+  long p1 = priority();
+  long p2 = ((const QueryRule &)r).priority();
+  if (p1 == p2)
+    return 0;
+  return p1 < p2 ? 1 : -1;
+}
+
+void
+ProcessingMode::QueryRule::trace(Collector &c) const
+{
+  if (nl_)
+    c.trace(nl_);
+}
+
 void ProcessingMode::addElementRule(NCVector<Pattern> &patterns,
-				    Owner<Expression> &expr,
-				    RuleType ruleType,
-				    const Location &loc,
-				    Interpreter &interp)
+			     Owner<Expression> &expr,
+			     RuleType ruleType,
+			     const Location &loc,
+			     Interpreter &interp)
 {
   Ptr<Action> action = new Action(interp.currentPartIndex(), expr, loc);
   for (size_t i = 0; i < patterns.size(); i++)
@@ -159,9 +251,9 @@ void ProcessingMode::addElementRule(NCVector<Pattern> &patterns,
 }
 
 void ProcessingMode::addRootRule(Owner<Expression> &expr,
-				 RuleType ruleType,
-				 const Location &loc,
-				 Interpreter &interp)
+			     RuleType ruleType,
+			     const Location &loc,
+			     Interpreter &interp)
 {
   Ptr<Action> action = new Action(interp.currentPartIndex(), expr, loc);
   Vector<Rule> &rules = rootRules_[ruleType];
@@ -180,23 +272,16 @@ void ProcessingMode::addRootRule(Owner<Expression> &expr,
   }
 }
 
-void ProcessingMode::addQueryRule(Owner<Expression> &query, 
-				  Owner<Expression> &expr,
-				  Owner<Expression> &priority,
-				  RuleType ruleType, 
-				  const Location &loc, 
-				  Interpreter &interp)
+void ProcessingMode::addQueryRule(Owner<Expression> &query,
+                             Owner<Expression> &expr,
+                             Owner<Expression> &priority,
+			     RuleType ruleType,
+			     const Location &loc,
+			     Interpreter &interp)
 {
-  IList<Pattern::Element> list;
-  Pattern::Element *elem = new Pattern::Element(StringC(), 0);
-  list.insert(elem);
-  elem->addQualifier(new Pattern::NodeQualifier(query, priority, 
-                                                this, &interp, loc));
-  Pattern pattern(list);
-  NCVector<Pattern> patterns(1);
-  patterns[0].swap(pattern);
-  addElementRule(patterns, expr, ruleType, loc, interp);
-  hasQuery_ = 1;
+  Ptr<Action> action = new Action(interp.currentPartIndex(), expr, loc);
+  queryRules_[ruleType].push_back(
+     new QueryRule(action, query, priority, (ProcessingMode *)this, &interp));
 }
 
 ProcessingMode::GroveRules::GroveRules()
@@ -210,7 +295,6 @@ ProcessingMode::ElementRules::ElementRules(const StringC &name)
 }
 
 // Specificity gives specificity of last match; get specificity of current match.
-
 const ProcessingMode::Rule *
 ProcessingMode::findMatch(const NodePtr &node,
 			  Pattern::MatchContext &context,
@@ -218,23 +302,14 @@ ProcessingMode::findMatch(const NodePtr &node,
 			  Specificity &specificity) const
 {
   GroveString gi;
-  if ((node->getGi(gi) == accessOK) || hasQuery()) {
-    // This is necessary, since root nodes may now go through
-    // findElementMatch() *and* findRootMatch(), which won't work
-    // if findElementMatch() directly modifies specificity.
-    Specificity sp(specificity);
-    const Rule *rule = 
-      findElementMatch(StringC(gi.data(), gi.size()), 
-	               node, context, mgr, sp);
-    if (rule) { 
-      specificity = sp;
-      return rule;
-    }
-  }
+  if (node->getGi(gi) == accessOK) 
+    return findElementMatch(StringC(gi.data(), gi.size()), 
+	                    node, context, mgr, specificity);
   NodePtr tem;
-  if (node->getOrigin(tem) != accessOK) {
+  if (node->getOrigin(tem) != accessOK) 
     return findRootMatch(node, context, mgr, specificity);
-  }
+  if (hasQuery())
+    return findQueryMatch(node, context, mgr, specificity);
   return 0;
 }
 
@@ -252,19 +327,38 @@ ProcessingMode::findElementMatch(const StringC &gi,
     for (;;) {
       const ProcessingMode &mode
 	= *(initial_ && specificity.toInitial_ ? initial_ : this);
-      if (!vecP) {
-	const GroveRules &gr = mode.groveRules(node, mgr);
-	const ElementRules *er = gr.elementTable.lookup(gi);
-	vecP = er ? er->rules : gr.otherRules;
-      }
-      const Vector<const ElementRule *> &vec = vecP[specificity.ruleType_];
-      ASSERT(specificity.nextRuleIndex_ <= vec.size());
-      for (size_t &i = specificity.nextRuleIndex_; i < vec.size(); i++) {
-	if (vec[i]->trivial() || vec[i]->matches(node, context)) {
-	  const Rule *rule = vec[i];
-	  elementRuleAdvance(node, context, mgr, specificity, vec);
-	  return rule;
-	}
+      for (;;) {
+        if (specificity.query_) { 
+          const Vector<QueryRule *> &vec = queryRules_[specificity.ruleType_];
+          ASSERT(specificity.nextRuleIndex_ <= vec.size());
+          for (size_t &i = specificity.nextRuleIndex_; i < vec.size(); i++) {
+	    if (vec[i]->matches(node)) {
+	      const Rule *rule = vec[i];
+	      queryRuleAdvance(node, mgr, specificity, vec);
+	      return rule;
+	    }
+          }
+          specificity.query_ = 0;
+          specificity.nextRuleIndex_ = 0;
+        }
+        else {
+          if (!vecP) {
+	    const GroveRules &gr = mode.groveRules(node, mgr);
+	    const ElementRules *er = gr.elementTable.lookup(gi);
+	    vecP = er ? er->rules : gr.otherRules;
+          }
+          const Vector<const ElementRule *> &vec = vecP[specificity.ruleType_];
+          ASSERT(specificity.nextRuleIndex_ <= vec.size());
+          for (size_t &i = specificity.nextRuleIndex_; i < vec.size(); i++) {
+	    if (vec[i]->trivial() || vec[i]->matches(node, context)) {
+	      const Rule *rule = vec[i];
+	      elementRuleAdvance(node, context, mgr, specificity, vec);
+	      return rule;
+	    }
+          }
+          specificity.query_ = 1;
+          break;
+        }
       }
       if (!initial_)
 	break;
@@ -292,11 +386,73 @@ ProcessingMode::findRootMatch(const NodePtr &node,
   for (;;) {
     for (;;) {
       const ProcessingMode &mode = *(initial_ && specificity.toInitial_ ? initial_ : this);
-      const Vector<Rule> &rules = mode.rootRules_[specificity.ruleType_];
-      if (specificity.nextRuleIndex_ < rules.size())
-	return &rules[specificity.nextRuleIndex_++];
+      for (;;) {
+        if (specificity.query_) {
+          const Vector<QueryRule *> &vec = queryRules_[specificity.ruleType_];
+          ASSERT(specificity.nextRuleIndex_ <= vec.size());
+          for (size_t &i = specificity.nextRuleIndex_; i < vec.size(); i++) {
+	    if (vec[i]->matches(node)) {
+	      const Rule *rule = vec[i];
+	      queryRuleAdvance(node, mgr, specificity, vec);
+	      return rule;
+	    }
+          }
+          specificity.query_ = 0;
+          specificity.nextRuleIndex_ = 0;
+        }
+        else {
+          const Vector<Rule> &rules = mode.rootRules_[specificity.ruleType_];
+          if (specificity.nextRuleIndex_ < rules.size())
+	    return &rules[specificity.nextRuleIndex_++];
+          specificity.query_ = 1;
+          break;
+        }
+      } 
       if (!initial_ || specificity.toInitial_)
-	break;
+        break;
+      specificity.nextRuleIndex_ = 0;
+      specificity.toInitial_ = 1;
+    }
+    if (specificity.ruleType_ == constructionRule)
+      break;
+    specificity.ruleType_ = constructionRule;
+    specificity.nextRuleIndex_ = 0;
+    specificity.toInitial_ = 0;
+    specificity.query_ = 1; // start looking at query construction rules
+  }
+  return 0;
+}
+
+const ProcessingMode::Rule *
+ProcessingMode::findQueryMatch(const NodePtr &node,
+			      Pattern::MatchContext &context,
+			      Messenger &mgr,
+			      Specificity &specificity) const
+{
+  for (;;) {
+    for (;;) {
+      const ProcessingMode &mode = *(initial_ && specificity.toInitial_ ? initial_ : this);
+      for (;;) {
+        if (specificity.query_) {
+          const Vector<QueryRule *> &vec = queryRules_[specificity.ruleType_];
+          ASSERT(specificity.nextRuleIndex_ <= vec.size());
+          for (size_t &i = specificity.nextRuleIndex_; i < vec.size(); i++) {
+	    if (vec[i]->matches(node)) {
+	      const Rule *rule = vec[i];
+	      queryRuleAdvance(node, mgr, specificity, vec);
+	      return rule;
+	    }
+          }
+          specificity.query_ = 0;
+          specificity.nextRuleIndex_ = 0;
+        }
+        else { 
+          specificity.query_ = 1;
+          break;
+        }
+      } 
+      if (!initial_ || specificity.toInitial_)
+        break;
       specificity.nextRuleIndex_ = 0;
       specificity.toInitial_ = 1;
     }
@@ -327,6 +483,7 @@ void ProcessingMode::elementRuleAdvance(const NodePtr &node,
 				        Specificity &specificity,
 				        const Vector<const ElementRule *> &vec)
 {
+  specificity.query_ = 0;
   size_t &i = specificity.nextRuleIndex_;
   if (specificity.ruleType_ != constructionRule) {
     ++i;
@@ -350,6 +507,36 @@ void ProcessingMode::elementRuleAdvance(const NodePtr &node,
     ++i;
   } while (i < vec.size()
            && vec[hit]->ElementRule::compareSpecificity(*vec[i]) == 0);
+}
+
+void ProcessingMode::queryRuleAdvance(const NodePtr &node,
+				      Messenger &mgr,
+				      Specificity &specificity,
+				      const Vector<QueryRule *> &vec)
+{
+  size_t &i = specificity.nextRuleIndex_;
+  if (specificity.ruleType_ != constructionRule) {
+    i++;
+    return;
+  }
+  size_t hit = i;
+  do {
+    ++i;
+    if (i >= vec.size()
+	|| vec[hit]->compareSpecificity(*vec[i]) != 0)
+      return;
+  } while (!vec[i]->matches(node));
+
+  const LocNode *lnp;
+  Location nodeLoc;
+  if ((lnp = LocNode::convert(node)) != 0
+      && lnp->getLocation(nodeLoc) == accessOK)
+    mgr.setNextLocation(nodeLoc);
+  mgr.message(InterpreterMessages::ambiguousMatch);
+  do {
+    ++i;
+  } while (i < vec.size()
+           && vec[hit]->compareSpecificity(*vec[i]) == 0);
 }
 
 #ifdef DSSSL_NAMESPACE

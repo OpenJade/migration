ee049d090bf0817c5c7e0455ca888305f384c4b2
Adam Di Carlo <adicarlo@users.sourceforge.net>
2001-02-08 05:44:43 +0000

    fix a c++ warning, warning: nsigned int SubstTable::operator [](unsigned int) const' was used before it was declared inline

--- sp/include/SubstTable.h
+++ sp/include/SubstTable.h
@@ -35,12 +35,6 @@ private:
   mutable bool isSorted_;
 };
 
-inline 
-void SubstTable::subst(Char &c) const
-{
-  c = operator[](c);
-}
-
 inline
 void SubstTable::subst(StringC &str) const
 {
@@ -55,6 +49,12 @@ Char SubstTable::operator[](Char t) const
     return lo_[t];
   else 
     return at(t);
+}
+
+inline 
+void SubstTable::subst(Char &c) const
+{
+  c = operator[](c);
 }
 
 #ifdef SP_NAMESPACE
